5acd9fa8c23b4a0f0d7da6b1c2fde032110cfa4c
Simeao jose <simeaojs@gmail.com>
2024-03-26 14:25:03 -0300

    ec2-bastion and security-bastion

---
+++ terraform/ec2_bastion.tf
@@ -0,0 +1,20 @@
+resource "aws_instance" "bastion" {
+
+  ami                         = var.ami_id
+  instance_type               = "t3.micro"
+  security_groups             = [aws_security_group.bastion_sg.id]
+  subnet_id                   = aws_subnet.subnet-public-a.id
+  key_name                    = var.keyname
+  associate_public_ip_address = true
+  tags = {
+    Name       = "bastion"
+    Project    = "PB UNICESUMAR"
+    CostCenter = "C092000024"
+  }
+
+  volume_tags = {
+    Name       = "bastion"
+    Project    = "PB UNICESUMAR"
+    CostCenter = "C092000024"
+  }
+}
---
+++ terraform/security_group_bastion.tf
@@ -0,0 +1,73 @@
+resource "aws_security_group" "bastion_sg" {
+  name        = "bastion_sg"
+  description = "Security group for bastion host"
+  vpc_id      = aws_vpc.vpc.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "Allow SSH access from anywhere"
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  tags = {
+    Name = "bastion_sg"
+  }
+
+}
+
+resource "aws_security_group" "private_ssh_sg" {
+  name        = "private_ssh_sg"
+  description = "Security group for bastion host"
+  vpc_id      = aws_vpc.vpc.id
+
+  ingress {
+    from_port       = 22
+    to_port         = 22
+    protocol        = "tcp"
+    security_groups = [aws_security_group.bastion_sg.id]
+    description     = "Allow SSH access from bastion host"
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "HTTP"
+  }
+
+  ingress {
+    from_port   = 2049
+    to_port     = 2049
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "NFS"
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "HTTPS"
+  }
+
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  tags = {
+    Name = "private_ssh_sg"
+  }
+
+}
